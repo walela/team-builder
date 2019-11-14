7bd9978487f76e3c9195bb83605d5430ddcd69bf
Austin Walela <austinmasibo@gmail.com>
2019-11-14 09:55:00 +0300

    set up form state and hooked to form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
+const initialTeam = [
+  {
+    firstName: "Austin",
+    lastName: "Walela",
+    email: "austinwalela@gmail.com",
+    role: "Backend Engineer"
+  },
+  {
+    firstName: "Colin",
+    lastName: "Toft",
+    email: "cappers86@gmail.com",
+    role: "Site Reliability Engineer"
+  },
+  {
+    firstName: "Jordan",
+    lastName: "Covo",
+    email: "jcovo@gmail.com",
+    role: "Frontend Engineer"
+  },
+  {
+    firstName: "Petar",
+    lastName: "Vlaisavljevic",
+    email: "ropeks@gmail.com",
+    role: "CTO"
+  }
+];
+
+const initialMemberForm = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  role: ""
+};
+
+function Member(props) {
+  const { firstName, lastName, email, role, key } = props;
+  return (
+    <div className="card">
+      <h3>{`${firstName} ${lastName}`}</h3>
+      <div>
+        <b>Email</b>: {email}
+      </div>
+      <div>
+        <b>Role</b>: {role}
+      </div>
+    </div>
+  );
+}
+
 function App() {
+  const [team, setTeam] = useState(initialTeam);
+  const [memberForm, setMemberForm] = useState(initialMemberForm);
+
+  const handleChange = e => {
+    setMemberForm({ ...memberForm, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTeam(team.concat(memberForm));
+    setMemberForm(initialMemberForm);
+  };
+
   return (
     <div className="App">
-      <h1>WEBEU4, Inc.</h1>
-      <Form data={{ email: "" }} />
+      <h1>WEBEU4, Inc</h1>
+      <div className="team-list">
+        {team.map(member => {
+          return (
+            <Member
+              firstName={member.firstName}
+              lastName={member.lastName}
+              email={member.email}
+              role={member.role}
+            />
+          );
+        })}
+      </div>
+      <Form data={memberForm} handleChange={handleChange} handleSubmit={handleSubmit} />
     </div>
   );
 }
