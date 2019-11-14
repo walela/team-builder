05820601e3ef2bff7b953d355fbe51ea7c4da478
Austin Walela <austinmasibo@gmail.com>
2019-11-14 09:45:06 +0300

    styled form

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -3,15 +3,29 @@ import React from "react";
 function Form(props) {
   const { firstName, lastName, email, role } = props.data;
   return (
-    <form>
-      <fieldset>
-        <legend>Add a Team Member</legend>
-        <input type="email" name="email" placeholder="Email Adress" value={email}></input>
-        <input type="text" name="firstName" placeholder="First Name" value={firstName}></input>
-        <input type="text" name="lastName" placeholder="Last Name" value={lastName}></input>
-        <input type="text" name="role" placeholder="Role" value={role}></input>
-      </fieldset>
-    </form>
+    <div className="form-container">
+      <form>
+        <fieldset>
+          <legend>Add a Team Member</legend>
+          <input type="email" name="email" placeholder="Email Adress" value={email}></input>
+          <input
+            type="text"
+            className="names first"
+            name="firstName"
+            placeholder="First Name"
+            value={firstName}
+          ></input>
+          <input
+            type="text"
+            className="names"
+            name="lastName"
+            placeholder="Last Name"
+            value={lastName}
+          ></input>
+          <input type="text" name="role" placeholder="Role" value={role}></input>
+        </fieldset>
+      </form>
+    </div>
   );
 }
 
